78232721d0a247a7ddb4007af1920bb91413a7f4
RovaHenryawan <rovahenryawan027@gmail.com>
2025-01-27 18:15:00 +0700

    Test Case 14

---
+++ WebComponent/RegisterPage.js
@@ -0,0 +1,25 @@
+const {By} = require('selenium-webdriver');
+
+class RegisterPage {
+    constructor(driver) {
+        this.driver = driver;
+        this.registerMenu = By.css("[href='/login']");
+        this.usernameInput = By.css("[name='name']");
+        this.emailInput = By.css("[data-qa='signup-email']");
+        this.signupInput = By.css("[data-qa='signup-button']");
+        this.signUpHeader = By.css(".signup-form > h2");
+    }
+
+    async register(username, email) {
+        await this.driver.findElement(this.usernameInput).sendKeys(username);
+        await this.driver.findElement(this.emailInput).sendKeys(email);
+        await this.driver.findElement(this.signupInput).click();
+    }
+
+    async verifySignUpHeader() {
+        const title = await this.driver.findElement(this.signUpHeader).getText();
+        return title;
+    }
+}
+
+module.exports = RegisterPage;
